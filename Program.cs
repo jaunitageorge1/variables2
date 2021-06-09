930cde45f752ca5d748c8d1e4fbc70f959c1b33d
jaunitageorge1 <jaunitageorge@gmail.com>
2021-06-08 23:40:42 -0400

    inital commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Variables2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Info");
+            string yourName = Console.ReadLine();
+            int yourAge = 5;
+            bool isFriend = true;
+            char myFav = '@';
+            decimal num1 = 21.5m;
+            double num2 = 21.5; 
+
+            Console.WriteLine($"Your Name is {yourName}. Your Age is {yourAge}. It is {isFriend} that you are a friend. Your favorite special character is the {myFav} symbol. You have a favorite number too, and in C#, that's written in decimal form like this: {num1}, and in double form like {num2}.");
+        }
+    }
+}
